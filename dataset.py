8937bc5b5b4d23b89fc87f24025ac38095844a4f
seuqaj114 <migjacques@hotmail.com>
2015-12-02 18:49:15 +0000

    Added face bounding box extraction.

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+from math import ceil
 from random import shuffle
 
 MAX_NUM_IMAGES = 1521
@@ -18,9 +19,11 @@ class BioIDDataProvider():
         else:
             self.index_list = range(self.num_images)
 
-        self.images    = []
-        self.eyes      = []
-        self.keypoints = []
+        self.images     = []
+        self.eyes       = []
+        self.keypoints  = []
+        self.faces      = []
+        self.faces_bbox = []
 
         self.IMG_PATH = os.path.abspath("assets/images")
         self.EYE_PATH = os.path.abspath("assets/eyepos")
@@ -29,15 +32,22 @@ class BioIDDataProvider():
         for index in self.index_list:
             #   Store eye positions
             eye_filename = os.path.join(self.EYE_PATH,"BioID_"+str(index).zfill(4)+".eye")
-            self.eyes.append(self.format_eye_file(eye_filename))
+            eyes = self.format_eye_file(eye_filename)
+            self.eyes.append(eyes)
 
             #   Store images
             image_filename = os.path.join(self.IMG_PATH,"BioID_"+str(index).zfill(4)+".pgm")
-            self.images.append(cv2.imread(image_filename,0))
+            image = cv2.imread(image_filename,0)
+            self.images.append(image)
 
             #   Store keypoints
             keypoints_filename = os.path.join(self.KP_PATH,"bioid_"+str(index).zfill(4)+".pts")
-            self.keypoints.append(self.format_keypoints_file(keypoints_filename))
+            keypoints = self.format_keypoints_file(keypoints_filename)
+            self.keypoints.append(keypoints)
+
+            face_bbox = self.extract_face_bbox(image, keypoints)
+            self.faces_bbox.append(face_bbox)
+
 
         self.__curr_index = 0
 
@@ -60,6 +70,43 @@ class BioIDDataProvider():
 
         return coords
 
+    def extract_face_bbox(self, image, keypoints):
+        margin = 10
+
+        x_locations = [kp[0] for kp in keypoints]
+        y_locations = [kp[1] for kp in keypoints]
+
+        face_bbox = [max(0,min(x_locations)-margin), max(0,min(y_locations)-margin*4), min(image.shape[1],max(x_locations)+margin), min(image.shape[0],max(y_locations)+margin)]
+
+        diff = (face_bbox[2] - face_bbox[0]) - (face_bbox[3] - face_bbox[1])
+        #   If box is higher than wider, increase width
+        if diff > 0:
+            #   We have to make sure the boxes don't go out of the image
+            face_bbox[1] = max(0,face_bbox[1]-abs(diff)/2)
+            face_bbox[3] = min(image.shape[0]-1,face_bbox[3]+int(ceil(abs(diff)/2.0)))
+
+            #   If it isn't possible to fully increase width, decrease height
+            diff2 = (face_bbox[2] - face_bbox[0]) - (face_bbox[3] - face_bbox[1])
+            if diff2 > 0:
+                face_bbox[0] += abs(diff2)/2
+                face_bbox[2] -= int(ceil(abs(diff2)/2.0))
+
+        #   Otherwise, increase height
+        else:
+            #   We have to make sure the boxes don't go out of the image
+            face_bbox[0] = max(0,face_bbox[0]-abs(diff)/2)
+            face_bbox[2] = min(image.shape[1]-1,face_bbox[2]+int(ceil(abs(diff)/2.0)))
+
+            #   If it isn't possible to fully increase height, decrease width
+            diff2 = (face_bbox[2] - face_bbox[0]) - (face_bbox[3] - face_bbox[1])
+            if diff2 < 0:
+                face_bbox[1] += abs(diff2)/2
+                face_bbox[3] -= int(ceil(abs(diff2)/2.0))
+
+        assert (face_bbox[2] - face_bbox[0]) - (face_bbox[3] - face_bbox[1]) == 0, "Bounding box must be a square."
+
+        return face_bbox
+
     def __iter__(self):
         return self
 
@@ -70,5 +117,3 @@ class BioIDDataProvider():
         except IndexError:
             self.__curr_index = 0
             raise StopIteration
-
-            
